3a154db07f3dcc3f9b6465b28a83ef2fa7a81efb
Daniel Grujic <DanielGT22@github.com>
2024-01-24 15:05:14 +0100

    first commit

---
+++ src/main/java/daniel/designPaterns/DesignPaternsApplication.java
@@ -0,0 +1,13 @@
+package daniel.designPaterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPaternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPaternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daniel/designPaterns/DesignPaternsApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.designPaterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPaternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
